4b41a184e4f02f2abfc0b3784bf0858e619eb25a
IvanMurabito <i.murabito@enerbrain.com>
2020-10-01 19:14:13 +0200

    working server (maybe)

--- server/src/main/java/it/polito/ai/virtualLabs/VirtualLabs.java
+++ server/src/main/java/it/polito/ai/virtualLabs/VirtualLabs.java
@@ -1,12 +1,11 @@
 package it.polito.ai.virtualLabs;
-
+import it.polito.ai.virtualLabs.entities.User;
 import it.polito.ai.virtualLabs.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
