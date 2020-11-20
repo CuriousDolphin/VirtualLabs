ec4c1a5d7979ead81d7a8842aeaded77de3e9817
Ivan <isnob46@gmail.com>
2020-11-20 20:54:18 +0100

    mini fix

--- server/src/main/java/it/polito/ai/virtualLabs/dtos/TeamDTO.java
+++ server/src/main/java/it/polito/ai/virtualLabs/dtos/TeamDTO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @Data
 public class TeamDTO {
-    //private Long id;
+    private Long id;
     private String name;
     private int status;
     private List<StudentDTO> members;
