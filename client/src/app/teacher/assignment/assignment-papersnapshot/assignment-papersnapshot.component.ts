595e887107607fd23c80186e7073dbcf29519221
jcoppo <jacopoverd@gmail.com>
2021-01-06 19:17:07 +0100

    add reader

--- client/src/app/teacher/assignment/assignment-papersnapshot/assignment-papersnapshot.component.ts
+++ client/src/app/teacher/assignment/assignment-papersnapshot/assignment-papersnapshot.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { PaperSnapshot } from 'src/app/models/papersnapshot.model';
 import { formatDate } from '@angular/common'
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 
 @Component({
@@ -16,12 +16,12 @@ export class AssignmentPapersnapshotComponent implements OnInit {
   formGroup: FormGroup;
   toReviewControl = new FormControl(true)
   voteControl = new FormControl({value: 1, disabled: this.toReviewControl.value}, [Validators.min(1), Validators.max(30)])
-  solutionControl = new FormControl("", [Validators.required])
-  solutionPath = ""
+  solutionControl = new FormControl(null, [Validators.required])
   imageLink: String = "http://localhost:8080/VM_images/defaultVmImage.png"
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private changeDetector: ChangeDetectorRef
   ) {
     this.formGroup = formBuilder.group({
       toReview: this.toReviewControl,
@@ -50,11 +50,29 @@ export class AssignmentPapersnapshotComponent implements OnInit {
     event.checked ? control.disable() : control.enable()
   }
 
-  changeInput() {
-    this.solutionPath = this.formGroup.get("solution").value
+  onFileChange(event) {
+
+    let reader = new FileReader();
+
+    if(event.target.files && event.target.files.length) {
+      const [file] = event.target.files
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        console.log(reader.result)
+        this.formGroup.patchValue({
+          solution: reader.result
+        })
+        this.changeDetector.markForCheck()
+      }
+    }
+  }
+
+  openImage() {
+    console.log("apro Immagine")
   }
 
   onSubmit(): void {
+    console.log("invio")
     console.log(this.formGroup.value)
     // qua va scritto il codice per mandare il file al client
   }
