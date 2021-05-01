747f17a91ffb82fee768c4f93265b867c560bd05
simone <simone.soncin.95@gmail.com>
2021-05-01 17:49:02 +0200

    resize registration photo, todo: send photo to server

--- client/src/app/auth/register-dialog/register-dialog.component.ts
+++ client/src/app/auth/register-dialog/register-dialog.component.ts
@@ -93,30 +93,11 @@ export class RegisterDialogComponent implements OnInit {
     if (this.authSubscription) this.authSubscription.unsubscribe();
   }
 
-  _onFileChange(event) {
-
-    let reader = new FileReader();
-
-    if (event.target.files && event.target.files.length) {
-      const [file] = event.target.files
-      reader.readAsDataURL(file)
-      reader.onload = () => {
-        this.imageSrc = reader.result as ArrayBuffer;
-        
-        //this.registerForm.patchValue({
-        //  solutionFileSource: reader.result
-        //})
-        this.changeDetector.markForCheck()
-      }
-    }
-  }
   onFileChange(event) {
 
     let reader = new FileReader();
-  
+
     if (event.target.files && event.target.files.length) {
-      var img = document.createElement("img");
-      var canvas = document.createElement('canvas');
       const [file] = event.target.files
       reader.readAsDataURL(file)
       reader.onload = () => {
@@ -125,46 +106,6 @@ export class RegisterDialogComponent implements OnInit {
         //this.registerForm.patchValue({
         //  solutionFileSource: reader.result
         //})
-        this.changeDetector.markForCheck()
-      }
-      reader.onloadend = () => {
-        console.log(reader.result.toString());
-        img.src = reader.result.toString();
-        console.log("111111" + img.src);
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-        
-        var MAX_WIDTH = 200;
-        var MAX_HEIGHT = 100;
-        var width = img.width;
-        var height = img.height;
-  
-        if (width > height) {
-          if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-          }
-        } else {
-          if (height > MAX_HEIGHT) {
-            width *= MAX_HEIGHT / height;
-            height = MAX_HEIGHT;
-          }
-        }
-        canvas.width = width;
-        canvas.height = height;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, width, height);
-        console.log("22222" + img.src);
-        var buf = new ArrayBuffer(img.src.length);
-        var bufView = new Uint16Array(buf);
-        console.log(img.src.length.toString());
-        for (var i=0, strLen=img.src.length; i < strLen; i++) {
-          bufView[i] = img.src.charCodeAt(i);
-          console.log(i.toString());
-        }
-        console.log("3333" + buf);
-        this.imageSrc = buf;//reader.result as ArrayBuffer;
-        
         this.changeDetector.markForCheck()
       }
     }
@@ -173,7 +114,7 @@ export class RegisterDialogComponent implements OnInit {
 
 
 
-//TODO validatore matricosa (s o d succeduta da solo numeri)
+//TODO validatore matricola (s o d succeduta da solo numeri)
 export function removeSpaces(control: AbstractControl) {
   if (control && control.value && !control.value.trim().length) {
     control.setValue('');
